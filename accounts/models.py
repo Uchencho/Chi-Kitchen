f50f23d9606d30be21ba785ef46cf206c5dd0597
Uchencho <aloziekelechi17@gmail.com>
2020-06-28 19:28:39 +0100

    cleaned up models

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.base_user import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
-    
+
     def _create_user(self, email, password, **extra_fields):
         """
         Creates and saves a user with the given email address and password
@@ -41,19 +41,3 @@ class User(AbstractUser):
 
     objects = CustomUserManager()
 
-# Create your models here.
-# class User(AbstractUser):
-#     email           = models.EmailField(_('email address'), unique=True)
-#     first_name      = models.CharField(_('first name'), max_length=30, blank=True)
-#     last_name       = models.CharField(_('last name'), max_length=30, blank=True)
-#     phone_number    = models.CharField(_('phone number'), max_length=14, blank=True)
-#     house_add       = models.TextField(_('house address'), blank=True)
-#     off_add         = models.TextField(_('Office address'), blank=True)
-#     last_login      = models.DateTimeField(_('last login'), auto_now_add=True, auto_now=True)
-#     date_joined     = models.DateTimeField(_('date joined'), auto_now_add=True)
-#     is_active       = models.BooleanField(_('active'), default=True)
-#     is_staff        = models.BooleanField(_('staff'), default=False)
-#     is_superuser    = models.BooleanField(_('superuser'), default=False)
-
-#     USERNAME_FIELD          = 'email'
-#     REQUIRED_FIELDS         = []
