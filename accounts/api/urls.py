f4a5fe6e7e11ee4fde6e7e7c1551546bdfb66054
Uchencho <aloziekelechi17@gmail.com>
2020-07-01 20:41:52 +0100

    logout not working yet

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,9 +1,15 @@
-from .views import RegisterAPIView, LoginView
+from .views import (
+                    RegisterAPIView, 
+                    LoginView, 
+                    LogoutView)
 from django.urls import path, include
+
+from oauth2_provider.views import RevokeTokenView
 
 app_name = "accounts"
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', RevokeTokenView.as_view(), name='logout'),
 ]
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,7 +4,8 @@ from .serializers import RegisterSerializer, LoginSerializer
 import requests
 from django.utils import timezone
 
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
+from rest_framework.authentication import get_authorization_header
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -60,3 +61,29 @@ class LoginView(APIView):
             user_model.save()
 
             return Response(content, status=status.HTTP_200_OK)
+
+
+class LogoutView(APIView):
+    permission_classes      = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        '''
+        Method to revoke tokens.
+        {"token": "<token>"}
+        '''
+        # r = requests.post(
+        #     'http://0.0.0.0:8000/o/revoke_token/', 
+        #     data={
+        #         'token': request.data['token'],
+        #         'client_id': CLIENT_ID,
+        #         'client_secret': CLIENT_SECRET,
+        #     },
+        # )
+        # # If it goes well return sucess message (would be empty otherwise) 
+        # if r.status_code == requests.codes.ok:
+        #     return Response({'message': 'token revoked'}, r.status_code)
+        # # Return the error if it goes badly
+        # return Response(r.json(), r.status_code)
+        # https://medium.com/@halfspring/guide-to-an-oauth2-api-with-django-6ba66a31d6d
+        print("helloooo", get_authorization_header(request))
+        return Response(status=status.HTTP_200_OK)
