583bdc18a8506052f1ff582365bba0ddbf1de9d1
Uchencho <aloziekelechi17@gmail.com>
2020-07-03 23:48:50 +0100

    created the dish table and linked it to admin

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Dish
 
 # Register your models here.
+class DishAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'price', 'active', 'dish_type', 'date_available', 'tag']
+
+admin.site.register(Dish, DishAdmin)
--- food/models.py
+++ food/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class DishQuerySet(models.QuerySet):
+    pass
+
+class DishManager(models.Manager):
+    def get_queryset(self):
+        return DishQuerySet(self.model, using=self._db)
+
+class Dish(models.Model):
+
+    DISH_CHOICES = [
+    ('Breakfast', 'Breakfast'),
+    ('Lunch', 'Lunch'),
+    ('Dinner', 'Dinner'),
+    ('Desert', 'Desert'),
+    ('Other', 'Other')
+        ]
+
+    name            = models.CharField(max_length=100, unique=True)
+    price           = models.IntegerField()
+    active          = models.BooleanField(default=True)
+    dish_type       = models.CharField(max_length=50, choices=DISH_CHOICES)
+    date_available  = models.DateField()
+    tag             = models.CharField(max_length=100, blank=True)
+
+    objects = DishManager()
+
+    def __str__(self):
+        return self.name
