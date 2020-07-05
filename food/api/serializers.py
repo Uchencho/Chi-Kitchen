c11342253db77d0477480ced80503c4cb405bdf0
Uchencho <aloziekelechi17@gmail.com>
2020-07-05 11:06:40 +0100

    can create order and validating data sent

--- food/api/serializers.py
+++ food/api/serializers.py
@@ -41,3 +41,58 @@ class OrderListSerializer(serializers.ModelSerializer):
         context = self.context['request']
         order_model = Order.objects.filter(customer_name=context.user).first()
         return order_model.updated.strftime("%d-%b-%Y %H:%M")
+
+
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+    customer_name     = serializers.SerializerMethodField(read_only=True)
+    dish              = serializers.CharField()
+
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            'customer_name',
+            'dish',
+            'time_of_order', 
+            'updated', 
+            'address', 
+            'total_cost',
+        ]
+
+    def get_customer_name(self, obj):
+        context = self.context['request']
+        return context.user.username
+
+    def validate_dish(self, value):
+        qs = Dish.objects.filter(name__iexact=value)
+        if not qs.exists():
+            raise serializers.ValidationError("Dish does not exist. Kindly create first.")
+        return value
+
+    def validate(self, data):
+        """
+        Validates that the total price is more than the price of the food
+        """
+        dish_name = data.get("dish")
+        total_cost = data.get("total_cost")
+        dish_model = Dish.objects.filter(name__iexact=dish_name).first()
+        if total_cost < dish_model.price:
+            raise serializers.ValidationError({"Total Cost" : "Total cost cannot be less than cost of dish"})
+        return data
+
+    def create(self, validated_data):
+        """
+        Overwrites the create method because of foreign key issues
+        """
+        context = self.context['request']
+        cus_ = context.user
+        dish_model = Dish.objects.filter(name__iexact=validated_data.get('dish')).first()
+        
+        ord_obj = Order.objects.create(
+            customer_name = cus_,
+            address = validated_data.get('address'),
+            dish = dish_model,
+            total_cost = validated_data.get('total_cost')
+        )
+        return ord_obj
--- food/api/views.py
+++ food/api/views.py
@@ -1,10 +1,30 @@
 from rest_framework import generics, status, permissions
 
 from food.models import Dish, Order
-from .serializers import OrderListSerializer
+from .serializers import OrderListSerializer, OrderCreateSerializer
 
-class UserOrders(generics.ListAPIView):
+class UserOrdersView(generics.ListAPIView):
+    """
+    List all the orders of a specific user
+    """
     serializer_class    = OrderListSerializer
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request":self.request}
+
+    def get_queryset(self):
+        """
+        Filter results to return only user's Orders
+        """
+        the_user = self.request.user
+        return Order.objects.filter(customer_name=the_user)
+
+
+class CreateOrderView(generics.CreateAPIView):
+    """
+    Create an order
+    """
+    serializer_class    = OrderCreateSerializer
 
     def get_serializer_context(self, *args, **kwargs):
         return {"request":self.request}
